eeeb19911befc42f8513f0849c378bf6544f31ca
Michael <mmcintyre@ebsco.com>
2020-05-19 22:21:23 -0400

    replaced the higher order component with renderprop method

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import withHover from "./withHover";
+import Hover from "./Hover";
 
 const styles = {
   container: {
@@ -24,19 +24,20 @@ const styles = {
   },
 };
 
-function Tooltip({text, children, hovering}) {
-    return (
-      <div style={styles.container}>
-        {hovering === true && <div style={styles.tooltip}>{text}</div>}
-        {children}
-    </div>
-    )
-  }
+export default function Tooltip({ text, children }) {
+  return (
+    <Hover>
+      {(hovering) => (
+        <div style={styles.container}>
+          {hovering === true && <div style={styles.tooltip}>{text}</div>}
+          {children}
+        </div>
+      )}
+    </Hover>
+  );
+}
 
 Tooltip.propTypes = {
   text: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-  hovering: PropTypes.bool.isRequired
 };
-
-export default withHover(Tooltip)
