f4050bfc93c042f882845f9c7581871ed92d7405
Michael <mmcintyre@ebsco.com>
2020-05-20 20:09:56 -0400

    started writing app theme

--- app/index.js
+++ app/index.js
@@ -2,14 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Battle from "./components/Battle";
-import Popular from "./components/Popular"
+import Popular from "./components/Popular";
+import { ThemeProvider } from './contexts/theme'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState((theme) => {
+          theme === "light" ? "dark" : "light";
+        });
+      },
+    };
+  }
+
   render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Battle />
+        </div>
+      </ThemeProvider>
     );
   }
 }
